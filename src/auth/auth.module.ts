0be4e3bf49a8086e4eab1ad2e28fd9f18b322929
dima <komiksmr99@gmail.com>
2026-05-22 22:36:47 +0300

    add jwtConfig

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: process.env.JWT_TOKEN_TIME as "1h"
-      }
-    })
+    JwtModule.register({}) //секрет и времчя жизни будет передоваться при создании токенов
   ],
   controllers: [AuthController],
   providers: [AuthService],
