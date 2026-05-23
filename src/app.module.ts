1c810a73ff1a0a4ff308ca6ea44b99797a5ddb30
dima <komiksmr99@gmail.com>
2026-05-23 12:17:18 +0300

    add jwtConfig2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'jwt.config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [jwtConfig] }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
