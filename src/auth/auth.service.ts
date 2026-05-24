a1ff9e7a878548fafe08e02ef3e8517ebf7dff4f
Maksim Nikonenko <myhak356@yandex.ru>
2026-05-24 19:23:27 +0300

    feat: implement POST /auth/logout endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+﻿import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return {
-      message: 'This action adds a new auth',
-      payload: createAuthDto,
-    };
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return {
-      message: `This action updates a #${id} auth`,
-      payload: updateAuthDto,
-    };
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  logout() {
+    return { message: 'Logged out successfully' };
   }
 }
