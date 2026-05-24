d1862fe11cd1ccb474f5f6cd5f4f12e82a7aa597
Maksim Nikonenko <myhak356@yandex.ru>
2026-05-24 19:57:23 +0300

    fix: add User entity, UsersService.findByEmail and bcrypt for login feature

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: process.env.JWT_TOKEN_TIME as "1h"
-      }
-    })
+        expiresIn: process.env.JWT_TOKEN_TIME as '1h',
+      },
+    }),
+    UsersModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
