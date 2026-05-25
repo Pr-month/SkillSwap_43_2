f34593d0e395ed14a937ba9cd6b733880525e6da
Babaskina Elena <babas.karabas@yandex.ru>
2026-05-25 12:41:28 +0300

    Add: findAll() method in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return {
       message: 'This action adds a new user',
@@ -11,8 +19,8 @@ export class UsersService {
     };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {
