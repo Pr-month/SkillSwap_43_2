149f0db3e098dfc7e58b3024ad6a24784c2ce1ce
Mikhail Chernyshev <lishainik@gmail.com>
2026-05-27 18:53:45 +0300

    fix jwt.strategy теперь исползует jwtConfig

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TJwtPayload } from '../auth.types';
+import { JwtConfig } from '../../config/jwt.config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private config : JwtConfig) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET ?? 'secret',
+      secretOrKey: config.secret,
     });
   }
 
