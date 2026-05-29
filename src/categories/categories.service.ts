383c06310bc56cd6ca6fc455567223c991963517
Sergey Panagushin <faceortable@gmail.com>
2026-05-29 18:00:23 +0300

    add GET categories endpoint

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IsNull, Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepository: Repository<Category>,
+  ) {}
+
+  async findAll(): Promise<Category[]> {
+    return this.categoriesRepository.find({
+      where: { parent: IsNull() },
+      relations: { children: true },
+    });
+  }
+}
