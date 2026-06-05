46c11b44584a29c736425a0206ca9cc18a421b11
Mikhail Chernyshev <lishainik@gmail.com>
2026-06-05 21:42:05 +0300

    feat: описал entity request

--- src/requests/entities/request.entity.ts
+++ src/requests/entities/request.entity.ts
@@ -1,1 +1,38 @@
-export class Request {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Status } from '../requests.enums';
+import { Skill } from '../../skills/entities/skill.entity';
+
+@Entity({
+  name: 'requests',
+})
+export class Request {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  sender: User;
+
+  @Column()
+  receiver: User;
+
+  @Column({ type: 'enum', enum: Status })
+  status: Status;
+
+  @Column()
+  offeredSkill: Skill;
+
+  @Column()
+  requestedSkill: Skill;
+
+  @Column()
+  isRead: boolean;
+}
