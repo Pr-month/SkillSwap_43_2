55c7b68ae0ec86cd6bff5989695584a7ac363200
dima <komiksmr99@gmail.com>
2026-06-09 00:36:13 +0300

    add/request_create

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,11 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Request } from './entities/request.entity';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { Skill } from '../skills/entities/skill.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class RequestsService {
-  create(createRequestDto: CreateRequestDto) {
-    return 'This action adds a new request';
+  constructor(
+    @InjectRepository(Request)
+    private readonly requestsRepository: Repository<Request>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    @InjectRepository(Skill)
+    private readonly skillsRepository: Repository<Skill>,
+  ) {}
+
+  async create(senderId: string, dto: CreateRequestDto): Promise<Request> {
+    const sender = await this.usersRepository.findOne({
+      where: { id: senderId },
+    });
+
+    if (!sender) {
+      throw new NotFoundException('User not found (');
+    }
+
+    const offeredSkill = await this.skillsRepository.findOne({
+      where: { id: dto.offeredSkillId },
+      relations: { owner: true },
+    });
+
+    if (!offeredSkill) {
+      throw new NotFoundException('Offered skill not found');
+    }
+
+    const requestedSkill = await this.skillsRepository.findOne({
+      where: { id: dto.requestedSkillId },
+      relations: { owner: true },
+    });
+
+    if (!requestedSkill) {
+      throw new NotFoundException('Request skill not found');
+    }
+    if (offeredSkill.owner.id !== sender.id) {
+      throw new ForbiddenException('You can offer only your skills');
+    }
+
+    if (requestedSkill.owner.id === sender.id) {
+      throw new ForbiddenException('Cannot create request to yourself');
+    }
+
+    const request = this.requestsRepository.create({
+      sender,
+      receiver: requestedSkill.owner,
+      offeredSkill,
+      requestedSkill,
+    });
+
+    return this.requestsRepository.save(request);
   }
 
   findAll() {
