26a6b2f4af0988b7cad25d01d16db6c86266982d
Natallia Paliukhovich-Karastsialiova <natalia.n.polyuhovich-karastelyova@sandboxer.ru>
2026-06-11 09:35:49 +0300

    unit tests: auth + guads

---
+++ src/auth/guards/jwt-auth.guard.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+describe('JwtAuthGuard', () => {
+  let guard: JwtAuthGuard;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [JwtAuthGuard],
+    }).compile();
+
+    guard = module.get<JwtAuthGuard>(JwtAuthGuard);
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+
+  it('exposes passport guard methods', () => {
+    expect(typeof guard.canActivate).toBe('function');
+    expect(typeof guard.handleRequest).toBe('function');
+  });
+});
