67cebc6907513cea988b10cd136d79559eb6db27
Babaskina Elena <babas.karabas@yandex.ru>
2026-06-13 12:50:11 +0300

    Add: FilesController

---
+++ src/files/files.enums.ts
@@ -0,0 +1,11 @@
+export enum MimeTypes {
+  JPG = 'image/jpeg',
+  PNG = 'image/png',
+  GIF = 'image/gif',
+}
+
+export enum FileSizes {
+  ONE_MB = 1024 * 1024,
+  TWO_MB = 2 * 1024 * 1024,
+  THREE_MB = 3 * 1024 * 1024,
+}
